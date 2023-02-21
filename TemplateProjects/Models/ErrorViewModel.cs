8a37a9472b7bf6ff3c3c85f3669d2cb39a2052f2
Firdy <trifirdyanto@gmail.com>
2023-02-21 23:16:40 +0700

    Initial commit

---
+++ TemplateProjects/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TemplateProjects.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
